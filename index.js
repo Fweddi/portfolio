28a5e1ee23bdd399f7d7e09c295e774947df7a40
Freddie <freddiepreece@outlook.com>
2019-06-08 18:25:16 +0100

    Add new text, mobile design, attribution and arrow for navigation

--- index.js
+++ index.js
@@ -1,59 +1,69 @@
 import React from "react";
 import { render } from "react-dom";
 import keillerFilePath from "./assets/keiller.gif";
-import { HeaderSection, Paragraph, KeillerGif, Redacted } from './index.style';
+import { HeaderSection, HeaderTop, HeaderBottom, Paragraph, KeillerGif, Redacted, Attribution, Arrow } from './index.style';
 
 const App = () => {
-
-    const RedactedInfo = {
-        info1: 'property', 
-        info2: 'Italianate gardens, chequerboard paving,', 
-        info3: 'columns and neoclassical facades…'
+    const infoObject = {
+        i1: 'property',
+        i2: 'Italianate gardens',
+        i3: 'chequerboard paving',
+        i4: 'neoclassical facades…',
+        attribution: '– Laura Grace Ford, psychogeographer',
+        link: 'http://lauragraceford.blogspot.com/'
     }
 
-    const [object, setObject] = React.useState(RedactedInfo);
+    const aDuration = (i) => i.length / 15;
 
-    // const storeRedactedInfoLength = {
+    const [info, setInfo] = React.useState(infoObject);
 
-    // }
-
-    const changeRedactedInfo = () => {
-        const newRedactedInfo = {
-            info1: 'digital ',
-            info2: 'Google, Amazon, Facebook.               ',
-            info3: '                                 ', 
+    const changeInfo = () => {
+        const newInfoObject = {
+            i1: 'digital ',
+            i2: ' targeted adverts ',
+            i3: ' messenger bubbles ',
+            i4: ' autocomplete fields…',
+            attribution: '– Freddie Preece, full stack developer',
+            link: ''
         }
-        setObject(newRedactedInfo);
-
+        setInfo(newInfoObject);
     }
-
-    // window.setTimeout(changeRedactedInfo, 5000);
 
     React.useEffect(() => {
-        const redactedTimeout = window.setTimeout(changeRedactedInfo, 5000);
+        const redactedTimeout = window.setTimeout(changeInfo, 17000);
         return () => window.clearTimeout(redactedTimeout);
     }, [])
 
     return (
         <HeaderSection>
-            <KeillerGif src={keillerFilePath} />
-
-            <Paragraph>
-                Networks of <Redacted duration='2'delay='4' children={object.info1} length={(object.info1).length}/> developments create zones of control, landscapes pulsing with symbolism and intent.
-            </Paragraph>
-
-            {/* <Paragraph>
-            As architecture becomes nomadic, there are encounters, the splintering of ideologies, belief and desires.
-            </Paragraph> */}
-
-            <Paragraph>
-                The signs are everywhere – you hear the faint crackling of power, notice where it lies… encoded, encrypted… not concentrated in the City of London or Canary Wharf, but dispersed, iridescent in <Redacted duration = '2' delay='10' children={object.info2} length={(object.info2).length}/> <Redacted duration = '3.5' delay='10' children={object.info3} length={(object.info3).length} />
-            </Paragraph>
-
-            <Paragraph>
-                – Laura Grace Ford
-            </Paragraph>
-
+            <HeaderTop>
+                <KeillerGif src={keillerFilePath} />
+            </HeaderTop>
+            <HeaderBottom>
+                <Paragraph>
+                    Networks of <Redacted duration={aDuration(info.i1)} delay='5' children={info.i1} length={info.i1.length} /> developments create zones of control, landscapes pulsing with symbolism and intent.
+                </Paragraph>
+                <Paragraph>
+                    The signs are everywhere – you hear the faint crackling of power, notice where it lies… encoded, encrypted… not concentrated in the City of London or Canary Wharf, but dispersed, iridescent in <Redacted
+                        duration={aDuration(info.i2)}
+                        delay={aDuration(info.i1) + 10}
+                        children={info.i2}
+                        length={info.i2.length} />, <Redacted
+                        length={info.i3.length}
+                        duration={aDuration(info.i3)}
+                        delay={aDuration(info.i1) + 10 + aDuration(info.i2)}
+                        children={info.i3}
+                    />, <Redacted
+                        length={info.i4.length}
+                        duration={aDuration(info.i4)}
+                        delay={aDuration(info.i1) + 10 + aDuration(info.i2) + aDuration(info.i3)}
+                        children={info.i4}
+                        href={info.link}
+                    />
+                </Paragraph>
+                <Attribution href={info.link}>{info.attribution}</Attribution>
+            </HeaderBottom>
+            <Arrow> ↓ </Arrow>
         </HeaderSection>
     )
 }
--- index.style.js
+++ index.style.js
@@ -1,25 +1,64 @@
-import styled, { keyframes } from 'styled-components';
+import styled, { keyframes } from "styled-components";
 
 const HeaderSection = styled.section`
-    width: 100vw;
-    height: 100vh;
-    display:flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-    `
-const Paragraph = styled.p`
-    font-family: 'IBM Plex Mono', monospace;
-    text-align: center;
-    padding: 0em 5em;
-    `;
-
-const KeillerGif = styled.img`
-    width: 25em;
-    padding: 2em;
+  width: 100vw;
+  min-height: 100vh;
+  display: flex;
+  flex-flow: column wrap;
+  justify-content: center;
+  align-items: center;
+  background-color: #FBF9FE;
 `;
 
-const animation = (props) => {
+const HeaderTop = styled.div`
+  width: auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const HeaderBottom = styled.div`
+  width: 90vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-flow: column;
+`;
+
+const Paragraph = styled.p`
+  font-family: "Cutive Mono", monospace;
+  font-size: 1rem;
+  text-align: center;
+  height: auto;
+  width: 100%;
+`;
+
+const KeillerGif = styled.img`
+  max-height: 40vh;
+  padding: 1em;
+`;
+
+const Attribution = styled.a`
+  color: black;
+  font-family: "Cutive Mono", monospace;
+`
+
+const Arrow = styled.p`
+  font-family: "Cutive Mono", monospace;
+  opacity: 0;
+  animation: ${props => blink(props)} 1s linear 20s 4 forwards;
+`
+
+const blink = props => {
+  return keyframes`
+	0% {opacity: 0}
+	49%{opacity: 0}
+  50% {opacity: 1}
+  100% {opacity: 1}
+`;
+};
+
+const animation = props => {
   return keyframes`
   0% { 
     width: 0%;
@@ -28,31 +67,58 @@ const animation = (props) => {
   100% {
     width: ${props.length}ch;
   }
-`}
+`;
+};
+
+const animationreverse = props => {
+  return keyframes`
+  0% { 
+    width: ${props.length}ch;
+    left: auto;
+    right: 0;
+  }
+
+  100% {
+    width: 0%;
+    left: auto;
+    right: 0;
+  }
+`;
+};
 
 const Redacted = styled.span`
-    position: relative;
-    font-family: 'IBM Plex Mono', monospace;
-    white-space: pre;
-    
-    ::after {
-          background: black;
-          border-radius: 0.1em;
-          box-shadow: 0 0 1px rgba(0,0,0,0.35);
-          content: "";
-          height: 1.2em;
-          left: 0;
-          display: inline-block;
-          width: 0%;
-          animation: ${props => animation(props)} 2s linear 2s forwards, ${props => animation(props)} 2s linear 6s reverse forwards;
-          position: absolute;
-          transform: skewY(-5deg) rotate(5deg);
-        }
-      `;
-    
+  position: relative;
+  font-family: "Cutive Mono", monospace;
+  white-space: pre;
+  pointer-events: ${props => props.pointerevents};
+  cursor: ${props => props.cursor};
+  text-decoration: none;
+  color: black;
 
-export { HeaderSection, Paragraph, KeillerGif, Redacted };
+  ::after {
+    background: black;
+    border-radius: 0.1em;
+    box-shadow: 0 0 1px rgba(0, 0, 0, 0.35);
+    content: "";
+    height: 1.2em;
+    left: 0;
+    display: inline-block;
+    width: 0%;
+    animation: ${props => animation(props)} ${props => props.duration}s linear ${props => props.delay}s
+        forwards,
+      ${props => animationreverse(props)} 2s linear 18s forwards;
+    position: absolute;
+    transform: skewY(-5deg) rotate(5deg);
+  }
+`;
 
-
-
-
+export {
+  HeaderTop,
+  HeaderBottom,
+  HeaderSection,
+  Paragraph,
+  KeillerGif,
+  Redacted,
+  Attribution,
+  Arrow
+};
